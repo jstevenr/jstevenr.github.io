50e9b7b7205537a70b6286e29cf13182660e838f
J Steven Raquel <jsteven.raquel8@gmail.com>
2017-08-20 15:50:59 -0700

    Major updates to imdb.Rmd (mainly plot changes) and some minor biographical changes.

--- _site.R
+++ _site.R
@@ -2,10 +2,13 @@
 suppressPackageStartupMessages({
   library(tidyverse)
   library(stringr)
+  library(ggplot2)
 })
 
 ## variables I need for my site
-
+movies <- read.csv("~/Documents/git-workspace/imdb-ratings/movie_metadata.csv",
+                   stringsAsFactors = FALSE, na.strings = c(NA, "NA"), skipNul = TRUE,
+                   fill = FALSE)
 
 ## knitr options I want set as default for all ('global') code chunks
 knitr::opts_chunk$set(echo = FALSE, message = FALSE, warning = FALSE)
